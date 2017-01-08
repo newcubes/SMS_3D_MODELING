577b11a050c2a15c7127c45350ff022eeb4d8f7b
Austin Wade Smith <newcubes@gmail.com>
2017-01-08 14:12:30 +0900

    Update FONA_SMS_SERIAL_CONTROL.ino

--- FONA_SMS_SERIAL_CONTROL.ino
+++ FONA_SMS_SERIAL_CONTROL.ino
@@ -1,5 +1,18 @@
 /*************************************************** 
   Modifications for Rhino COM by Austin Wade SMith
+  
+  -  This is an example for our Adafruit FONA Cellular Module
+ -  Designed specifically to work with the Adafruit FONA 
+ -  ----> http://www.adafruit.com/products/1946
+ -  ----> http://www.adafruit.com/products/1963
+ -  ----> http://www.adafruit.com/products/2468
+ -  ----> http://www.adafruit.com/products/2542
+ -  These cellular modules use TTL Serial to communicate, 2 pins are 
+ -  required to interface
+ -  Adafruit invests time and resources providing this open source code, 
+ -  please support Adafruit and open-source hardware by purchasing 
+ -  products from Adafruit!
+
   Orignal code written by Limor Fried/Ladyada for Adafruit Industries.  
   BSD license, all text above must be included in any redistribution
  ****************************************************/
